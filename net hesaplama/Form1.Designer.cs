319fdd6a5139a852057c5e03f1a6fe379156ee66
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-02 14:57:54 +0300

    Add files via upload

---
+++ net hesaplama/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace net_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNet = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.txtDogru = new System.Windows.Forms.TextBox();
+            this.txtYanlis = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(24, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soru sayısı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(56, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Doğru";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(56, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yanlış";
+            // 
+            // lblNet
+            // 
+            this.lblNet.AutoSize = true;
+            this.lblNet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNet.Location = new System.Drawing.Point(24, 206);
+            this.lblNet.Name = "lblNet";
+            this.lblNet.Size = new System.Drawing.Size(54, 20);
+            this.lblNet.TabIndex = 3;
+            this.lblNet.Text = "Netleri";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(211, 177);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(127, 80);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "Hesaplayınız";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Location = new System.Drawing.Point(115, 40);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(114, 20);
+            this.txtSayi.TabIndex = 5;
+            // 
+            // txtDogru
+            // 
+            this.txtDogru.Location = new System.Drawing.Point(115, 75);
+            this.txtDogru.Name = "txtDogru";
+            this.txtDogru.Size = new System.Drawing.Size(114, 20);
+            this.txtDogru.TabIndex = 6;
+            // 
+            // txtYanlis
+            // 
+            this.txtYanlis.Location = new System.Drawing.Point(115, 111);
+            this.txtYanlis.Name = "txtYanlis";
+            this.txtYanlis.Size = new System.Drawing.Size(114, 20);
+            this.txtYanlis.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 365);
+            this.Controls.Add(this.txtYanlis);
+            this.Controls.Add(this.txtDogru);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblNet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNet;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.TextBox txtDogru;
+        private System.Windows.Forms.TextBox txtYanlis;
+    }
+}
+
---
+++ net hesaplama/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace net_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int dogru, yanlis, sayi;
+            double net;
+            dogru = Convert.ToInt32(txtDogru.Text);
+            yanlis = Convert.ToInt32(txtYanlis.Text);
+            sayi = Convert.ToInt32(txtSayi.Text);
+
+            net = (double)dogru-(yanlis/4.0);
+            lblNet.Text = "Netleri: " + net.ToString();
+            
+        }
+    }
+}
